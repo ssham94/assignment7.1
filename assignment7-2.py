7c0d0f6ea612d41f5d875af1b9e78fd851392a6d
Stanley Sham <stanley.sham94@gmail.com>
2019-07-10 11:00:47 -0400

    finished assignment 7.2

---
+++ assignment7-2.py
@@ -0,0 +1,38 @@
+documentary = "Chernobyl"
+drama = "Dunkirk"
+comedy = "Disaster Artist"
+dramedy = "Lala Land"
+
+likes_drama = None
+likes_documentary = None
+likes_comedy = None
+
+print("How would you rate documentaries? (input 1-5)")
+likes_documentary = int(input())
+
+print("How would you rate dramas? (input 1-5)")
+likes_drama = int(input())
+
+print("How would you rate comedies? (input 1-5)")
+likes_comedy = int(input())
+
+scores = {'Documentary': likes_documentary, 'Drama': likes_drama, 'Comedy': likes_comedy}
+
+highest_score = max(scores, key=scores.get)
+print(highest_score)
+
+if likes_documentary > 3:
+    print("I recommend watching Chernobyl")
+elif likes_drama > 3 and likes_comedy > 3:
+    print("I recommend watching Lala Land")
+elif likes_drama > 3:
+    print("I recommend watching Dunkirk")
+elif likes_comedy > 3:
+    print("I recommend watching Disaster Artist")
+elif highest_score == "Documentary":
+    print("I recommend watching Chernobyl")
+elif highest_score == "Drama":
+    print("I recommend watching Dunkirk")
+elif highest_score == "Comedy":
+    print("I recommend watching Disaster Artist")
+
